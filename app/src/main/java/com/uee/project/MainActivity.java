c2f4f3a43613f44d4ad9f1a83b19907635f439a0
Pabasaraa <pabasara.was@gmail.com>
2022-10-31 09:26:15 +0530

    Created Artcile card view, and article fragment with recycler view to hold article card

--- app/src/main/java/com/uee/project/MainActivity.java
+++ app/src/main/java/com/uee/project/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -14,11 +15,24 @@ public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         overridePendingTransition( android.R.anim.fade_in, android.R.anim.fade_out );
         setContentView ( R.layout.activity_main );
+
+        //Hide nav bar and status bar
+        //TODO: This will let an empty space under my bottom navigation after hiding the nav also status bar leave a space
+//        View decorView = getWindow().getDecorView();
+//        int uiOptions =
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+//                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+//                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+//        decorView.setSystemUiVisibility(uiOptions);
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
