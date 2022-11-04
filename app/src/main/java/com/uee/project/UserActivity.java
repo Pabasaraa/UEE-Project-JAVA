7eef739b01527e7d661c76df083c475968c00243
chavikulasekara <chavikulasekara@gmail.com>
2022-11-04 22:43:53 +0530

    add the java file and xml file

---
+++ app/src/main/java/com/uee/project/UserActivity.java
@@ -0,0 +1,50 @@
+package com.uee.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class UserActivity extends AppCompatActivity {
+    private TabLayout TabLayout;
+    private ViewPager viewPager;
+    private ImageView imageView2;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+
+        TabLayout = findViewById(R.id.TabLayout);
+        viewPager = findViewById(R.id.viewPager);
+        imageView2 = findViewById(R.id.imageView2);
+
+        TabLayout.setupWithViewPager(viewPager);
+
+        ViewPagerAdapter vpAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new Ganarate_UnitsFragment(),"UnitS");
+        vpAdapter.addFragment(new RepairFragment(),"REPAIR");
+        vpAdapter.addFragment(new FeedbackFragment(),"FEEDBACK");
+
+        viewPager.setAdapter(vpAdapter);
+
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//
+
+
+            }
+        });
+
+    }
+}
